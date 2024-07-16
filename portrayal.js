b4ce57e84744910ab8b116207fcfa4e3a8e31bbb
Vibhuti Bafna <vibhutibafna@Vibhutis-MacBook-Air.local>
2024-07-17 01:43:45 +0530

    hopefully final css fix

--- portrayal.js
+++ portrayal.js
@@ -1,32 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Video thumbnails
-    const videos = document.querySelectorAll('.video-thumbnail video');
-    if (videos.length) {
-        videos.forEach(video => {
-            video.muted = true; // Ensure video is muted to allow autoplay
-            video.pause(); // Ensure video is paused initially
-            
-            const videoOptions = {
-                threshold: 0.75 // Adjust the threshold as needed
-            };
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
+    
+    // Fade-in sections
+    const faders = document.querySelectorAll('.fade-in-section');
+    console.log('Fade-in sections:', faders);
+    
+    if (faders.length) {
+        const appearOptions = {
+            threshold: 0.1,
+            rootMargin: "0px 0px -50px 0px"
+        };
 
-            const playOnScroll = new IntersectionObserver(function(entries) {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        video.play();
-                    } else {
-                        video.pause();
-                        video.currentTime = 0;
-                    }
-                });
-            }, videoOptions);
-            
-            // Start observing after the initial load
-            setTimeout(() => {
-                playOnScroll.observe(video);
-            }, 500); // Delay the observer start by 500ms
+        const appearOnScroll = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    console.log('Element is intersecting:', entry.target);
+                    entry.target.classList.add('is-visible');
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, appearOptions);
+
+        faders.forEach(fader => {
+            appearOnScroll.observe(fader);
         });
     }
 
-    console.log('Script loaded successfully.');
+    // Video thumbnails
+    const videos = document.querySelectorAll('.video-thumbnail video');
+    console.log('Video thumbnails:', videos);
+
+    if (videos.length) {
+        videos.forEach(video => {
+            video.muted = true; // Ensure video is muted to allow autoplay
+            video.addEventListener('mouseover', () => {
+                console.log('Mouse over video:', video);
+                video.play();
+            });
+            video.addEventListener('mouseout', () => {
+                console.log('Mouse out video:', video);
+                video.pause();
+                video.currentTime = 0;
+            });
+        })
+    }
 });
